e86c10d708bdae4b4fbabc0da8780eefa2b3af8d
gabriel-abn <gabriel.ab.nascimento@gmail.com>
2024-08-09 23:22:07 -0300

    ✨ feat: add get method to RideRepository

--- src/application/protocols/repositories/ride-repository.ts
+++ src/application/protocols/repositories/ride-repository.ts
@@ -2,4 +2,5 @@ import type { Ride } from "src/domain/entities/ride";
 
 export interface IRideRepository {
 	save(ride: Ride): Promise<void>;
+	get(filter: Partial<{ rideId: string; passengerId: string; driverId: string }>): Promise<Ride[]>;
 }
--- src/presentation/repositories/ride-repository.ts
+++ src/presentation/repositories/ride-repository.ts
@@ -1,5 +1,6 @@
 import type { IRideRepository } from "src/application/protocols/repositories";
-import type { Ride } from "src/domain/entities/ride";
+import { Ride } from "src/domain/entities/ride";
+import { Coordinate } from "src/domain/valueObjects/coordinate";
 import type RelationalDatabase from "../common/relational-database";
 
 export class RideRepository implements IRideRepository {
@@ -36,6 +37,92 @@ export class RideRepository implements IRideRepository {
 			}
 		} catch (error: any) {
 			throw new Error(`RideRepository: Failed to save ride. ${error.message}`);
+		}
+	}
+
+	async get(filter: Partial<{ rideId: string; passengerId: string; driverId: string }>): Promise<Ride[]> {
+		try {
+			if (filter.rideId) {
+				const { rows } = await this.relationalDatabase.query(
+					`
+						SELECT ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long,
+						fare, distance, status, request_date
+						FROM app.ride
+						WHERE ride_id = $1;
+					`,
+					[filter.rideId],
+				);
+
+				return rows.map((row: any) =>
+					Ride.restore({
+						rideId: row.ride_id,
+						passengerId: row.passenger_id,
+						driverId: row.driver_id,
+						pickupLocation: new Coordinate({ latitude: Number(row.from_lat), longitude: Number(row.from_long) }),
+						dropoffLocation: new Coordinate({ latitude: Number(row.to_lat), longitude: Number(row.to_long) }),
+						fare: row.fare,
+						distance: row.distance,
+						status: row.status,
+						requestDate: row.request_date,
+					}),
+				);
+			}
+
+			if (filter.driverId) {
+				const { rows } = await this.relationalDatabase.query(
+					`
+						SELECT ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long,
+						fare, distance, status, request_date
+						FROM app.ride
+						WHERE passenger_id = $1;
+					`,
+					[filter.passengerId],
+				);
+
+				return rows.map((row: any) =>
+					Ride.restore({
+						rideId: row.ride_id,
+						passengerId: row.passenger_id,
+						driverId: row.driver_id,
+						pickupLocation: new Coordinate({ latitude: row.from_lat, longitude: row.from_long }),
+						dropoffLocation: new Coordinate({ latitude: row.to_lat, longitude: row.to_long }),
+						fare: row.fare,
+						distance: row.distance,
+						status: row.status,
+						requestDate: row.request_date,
+					}),
+				);
+			}
+
+			if (filter.passengerId) {
+				const { rows } = await this.relationalDatabase.query(
+					`
+						SELECT ride_id, passenger_id, driver_id, from_lat, from_long, to_lat, to_long,
+						fare, distance, status, request_date
+						FROM app.ride
+						WHERE passenger_id = $1;
+						`,
+					[filter.passengerId],
+				);
+
+				return rows.map((row: any) =>
+					Ride.restore({
+						rideId: row.ride_id,
+						passengerId: row.passenger_id,
+						driverId: row.driver_id,
+						pickupLocation: new Coordinate({ latitude: row.from_lat, longitude: row.from_long }),
+						dropoffLocation: new Coordinate({ latitude: row.to_lat, longitude: row.to_long }),
+						fare: row.fare,
+						distance: row.distance,
+						status: row.status,
+						requestDate: row.request_date,
+					}),
+				);
+			}
+
+			throw new Error("RideRepository: Missing filter.");
+		} catch (error: any) {
+			throw new Error(`RideRepository: Failed to get ride. ${error.message}`);
 		}
 	}
 }
